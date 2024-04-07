b38618705b8e9e017b187ac996fb1772d7fbd823
liulhong <51956067+hyrtee@users.noreply.github.com>
2024-04-07 22:44:30 +0800

    Add func function
    Signed-off-by: liulhong <51956067+hyrtee@users.noreply.github.com>

--- hello.c
+++ hello.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 
+static int func(int a)
+{
+	int b = 1;
+
+	a += b;
+
+	return a;
+}
+
 int main()
 {
 	size_t i, sum = 5;
 
+	sum = func(sum);
 	for (i = 0; i < sum; ++i) {
 		printf("hello world\n");
 	}
